adcc06c10decf31bcb86fe3def7bf04adce044e4
Suvadeep Ghoshal <ghoshalsuvadeep594@gmail.com>
2024-01-02 20:30:40 +0000

    TypeScript support added for react

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -1,9 +1,9 @@
+import Home from "./Home";
 import React from "react";
 
 const App = () => (
     <div>
-        <h1>App</h1>
-        <p>Find me in app/javascript/components/App.jsx</p>
+        <Home/>
     </div>
 );
 export default App
--- app/javascript/components/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import App from "./App";
-import {createRoot} from "react-dom/client";
-import {BrowserRouter} from "react-router-dom";
-
-document.addEventListener("turbo:load", () => {
-    const div = createRoot(document.body.appendChild(document.createElement("div")));
-    div.render(<App/>)
-});
