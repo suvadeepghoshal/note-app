d71a91c7d3aade7bda9a53a4aeb1a83f718c44d6
Suvadeep Ghoshal <ghoshalsuvadeep594@gmail.com>
2024-01-02 19:28:00 +0000

    Integrating basic react with rails app

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => (
+    <div>
+        <h1>App</h1>
+        <p>Find me in app/javascript/components/App.jsx</p>
+    </div>
+);
+export default App
---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Find me in app/javascript/components/Home.jsx</p>
+    </div>
+  );
+}
