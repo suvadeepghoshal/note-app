0b35e5fd143f68491ef0e15937acf91b47f2f23f
Suvadeep Ghoshal <ghoshalsuvadeep594@gmail.com>
2024-01-05 21:11:32 +0000

    Creating and saving Note in the DB for POST call and getting all Notes from the DB for GET call

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -1,4 +1,29 @@
 class Api::V1::NotesController < ApplicationController
   def index
+    @notes = Note.all
+    render json: @notes, status: :ok
+  end
+
+  def create
+    existing_note = Note.find_by(title: note_params[:title])
+    if existing_note
+      render json: { message: "Note with the same title already exists", type: "error" }, status:
+        :ok
+    else
+      note = Note.new(note_params)
+      if note.save
+        render json: note, status:
+          :created
+      else
+        render json: note.errors
+        :unprocessable_entity
+      end
+    end
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :content)
   end
 end
