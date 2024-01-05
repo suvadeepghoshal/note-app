68187559b91a3d6ee8f84e7177fdabf638bc062b
Suvadeep Ghoshal <ghoshalsuvadeep594@gmail.com>
2024-01-05 21:12:30 +0000

    Removing console logs

--- app/javascript/components/Layout.tsx
+++ app/javascript/components/Layout.tsx
@@ -3,7 +3,6 @@ import React from "react";
 
 export default function Layout() {
     const location = useLocation();
-    console.log(location);
     return (
         <div>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
