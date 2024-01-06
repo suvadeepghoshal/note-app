348c89542ece46798802abfe99fb4b10559a27ac
Suvadeep Ghoshal <ghoshalsuvadeep594@gmail.com>
2024-01-06 22:47:25 +0000

    Integrating react-hook-form on bootstrap component (it is not working)

--- app/javascript/components/Note.tsx
+++ app/javascript/components/Note.tsx
@@ -1,9 +1,36 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {NoteRS} from "../lib/types/NoteRS";
 import axios from "axios";
+import {
+    Button,
+    Form,
+    FormFeedback,
+    FormGroup,
+    Input,
+    Label,
+    Modal,
+    ModalBody,
+    ModalFooter,
+    ModalHeader
+} from "reactstrap";
+import {SubmitHandler, useForm} from "react-hook-form";
 
 export default function Note() {
-    const [notes, setNotes] = React.useState<NoteRS[]>([]);
+    const [notes, setNotes] = useState<NoteRS[]>([]);
+    const [modal, setModal] = useState(false);
+    type Input = { title: string, content: string }
+    const {register, handleSubmit, formState: {errors}, watch} = useForm<Input>();
+
+    const onSubmit: SubmitHandler<Input> = data => {
+        console.log("coming inside");
+        try {
+            console.log(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const toggle = () => setModal(!modal);
     useEffect(() => {
         console.log("Note component mounted");
         const fetchNotes = async () => {
@@ -19,19 +46,71 @@ export default function Note() {
             console.log("Note component unmounted");
         }
     }, []);
+
+    const NoteCard = ({note}: { note: NoteRS }) => (
+        <div className="card" style={{width: "18rem"}} key={note.id}>
+            <div className="card-body">
+                <h5 className="card-title">{note.title}</h5>
+                <h6 className="card-subtitle mb-2 text-body-secondary">Tags will be added later</h6>
+                <p className="card-text">{note.content}</p>
+                <a href="#" className="card-link">Edit</a>
+                <a href="#" className="card-link">Delete</a>
+            </div>
+        </div>
+    );
+
+    const closeBtn = (
+        <button className="btn-close" onClick={toggle} type="button">
+        </button>
+    );
+
+    const handleCreateNote = () => {
+        console.log("button clicked");
+    };
+
+    const CreateNote = () => <Modal isOpen={modal} toggle={toggle} className="modal-dialog modal-dialog-centered">
+        <ModalHeader toggle={toggle} close={closeBtn}>
+            Note Information
+        </ModalHeader>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <ModalBody>
+                <FormGroup>
+                    <Label for="title">Title</Label>
+                    <Input id="note_name" {...register("title", {required: "Title is required", maxLength: 20})}/>
+                    {errors.title?.type === "required" && <FormFeedback>
+                        {errors.title?.message}
+                    </FormFeedback>}
+                </FormGroup>
+                <FormGroup>
+                    <Label for="content">Content</Label>
+                    <Input
+                        id="note_content"
+                        type="textarea"
+                        {...register("content", {required: "Content is required"})}
+                    />
+                    {errors.content && <FormFeedback>
+                        {errors.content.message}
+                    </FormFeedback>}
+                </FormGroup>
+            </ModalBody>
+            <ModalFooter>
+                <Button color="success" type="submit">
+                    Create
+                </Button>{' '}
+                <Button color="danger" onClick={toggle}>
+                    Cancel
+                </Button>
+            </ModalFooter>
+        </form>
+    </Modal>
+
     return (
         <div className="container m-4">
-            {notes.map(note => (
-                <div className="card" style={{width: "18rem"}} key={note.id}>
-                    <div className="card-body">
-                        <h5 className="card-title">{note.title}</h5>
-                        <h6 className="card-subtitle mb-2 text-body-secondary">Tags will be added later</h6>
-                        <p className="card-text">{note.content}</p>
-                        <a href="#" className="card-link">Edit</a>
-                        <a href="#" className="card-link">Delete</a>
-                    </div>
-                </div>
-            ))}
+            <CreateNote/>
+            <Button color="primary" className="mb-4 float-end" onClick={toggle}>
+                Create Note
+            </Button>
+            {notes.map(note => <NoteCard key={note.id} note={note}/>)}
         </div>
 
     );
